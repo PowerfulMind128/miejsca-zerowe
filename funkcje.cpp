072eb4abf820fdf44ec078e1f4a5309302ba58a0
student <student@it12-16.it.local>
2021-11-09 11:09:25 +0100

    Dokonczenie naprawy konfliktow

--- funkcje.cpp
+++ funkcje.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -29,35 +28,5 @@ int oblicz_mz(double a, double b, double& wynik)
 		}
 	}
 }
-=======
-#include <iostream>
-using namespace std;
 
-/*
-zwraca kody wyjscia:
-0 - brak miejsc zerowych
-1 - jedno m.z.
-2 - nieskonczenie wiele m.z.
 
-zmiennej "wynik" przypisuje sie wartosc tylko przy zwroceniu 1
-*/
-int oblicz_mz(double a, double b, double& wynik)
-{
-	if(a!=0) //jedno m.z.
-	{
-		wynik = -b/a;
-		return 1;	
-	}
-	else
-	{
-		if(b==0) //a==0 i b==0: nieskonczenie wiele m.z. (prosta y=0)
-		{
-			return 2;
-		}
-		else //a==0 i b!=0: brak m.z. (prosta rownolegla do OX niepokrywajaca sie z nia)
-		{
-			return 0;
-		}
-	}
-}
->>>>>>> 4676bff69c2c93b6ba5d77170d7e37889ab20ff7
